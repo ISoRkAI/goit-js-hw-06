600e9cad31219788f95d2df82029876a39cfaa10
Dmytro Soroka <dimasoroka17@gmail.com>
2022-08-07 00:17:24 +0300

    task-4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const counterValue = {
+    value: 0,
+};
+
+const valueEl = document.querySelector("#value");
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+decrementBtn.addEventListener("click", function () {
+    counterValue.value -= 1;
+    valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener("click", function () {
+    counterValue.value += 1;
+    valueEl.textContent = counterValue.value;
+});
