7da17969605f6ba9934f0810e2e893fd7dcb2583
Dmytro Soroka <dimasoroka17@gmail.com>
2022-08-06 18:49:23 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+const ingredientItem = document.createElement('li');
+ingredientItem.classList.add('item');
+ingredientItem.textContent = "Potatoes";
+const ingredientItemTwo = document.createElement('li');
+ingredientItemTwo.classList.add('item');
+ingredientItemTwo.textContent = "Mushrooms";
+const ingredientItemThree = document.createElement('li');
+ingredientItemThree.classList.add('item');
+ingredientItemThree.textContent = "Garlic";
+const ingredientItemFour = document.createElement('li');
+ingredientItemFour.classList.add('item');
+ingredientItemFour.textContent = "Tomatos";
+const ingredientItemFive = document.createElement('li');
+ingredientItemFive.classList.add('item');
+ingredientItemFive.textContent = "Herbs";
+const ingredientItemSix = document.createElement('li');
+ingredientItemSix.classList.add('item');
+ingredientItemSix.textContent = "Condiments";
+list.append(ingredientItem, ingredientItemTwo, ingredientItemThree, ingredientItemFour, ingredientItemFive, ingredientItemSix)
