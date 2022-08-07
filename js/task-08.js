3a4683d980ccf8d27a5799150c3c8e688386ada6
Dmytro Soroka <dimasoroka17@gmail.com>
2022-08-07 15:24:24 +0300

    task - 8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const registerForm = document.querySelector(".login-form")
 
+registerForm.addEventListener("submit", handleSubmit);
+
+const users = [];
+const user = {};
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value && password.value) {
+        const data = {
+            email: email.value,
+            password: password.value
+        };
+        console.log(data)
+        event.currentTarget.reset()
+        return
+    }
+    alert('Все поля должны быть заполнены')
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorBtn = document.querySelector(".change-color");
+const colorBg = document.querySelector(".color");
+const bodyEl = document.querySelector("body")
+console.log(bodyEl)
+
+colorBg.textContent = '#ffffff';
+
+colorBtn.addEventListener('click', onColorBtn);
+
+function onColorBtn(event) {
+  event.preventDefault();
+  const newcolor = getRandomHexColor();
+  bodyEl.style.backgroundColor = newcolor;
+  colorBg.textContent = `${newcolor}`;
+  console.log(newcolor)
+}
